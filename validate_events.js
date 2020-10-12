be3cfae42dd92136a44a7af68173ea3761cc4a38
Yuan ZHOU <yuanzhou.cq@gmail.com>
2020-10-13 01:02:31 +0800

    add event validation

---
+++ validate_events.js
@@ -0,0 +1,21 @@
+const assert = require('assert')
+const fs = require('fs')
+
+let events = fs.readFileSync('events.json').toString()
+
+events = JSON.parse(events)
+
+events.forEach(e => {
+    console.log('Verifying title:', e.title)
+    assert(e.title)
+    console.log('Verifying location:', e.location)
+    assert(e.location)
+    console.log('Verifying start time:', e.start)
+    assert(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/.test(e.start))
+    console.log('Verifying end time:', e.end)
+    assert(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/.test(e.end))
+    console.log('Verifying url:', e.url)
+    assert(e.url === "" || e.url)
+})
+
+console.log('All OK.')
